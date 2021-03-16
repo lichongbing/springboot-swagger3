f278601e9eacd20d62502cef951777175aae6ff1
lichongbing <873610008@qq.com>
2021-03-16 09:22:09 +0900

    init

---
+++ src/main/java/cn/ruiyeclub/service/impl/ServiceImpl.java
@@ -0,0 +1,10 @@
+package cn.ruiyeclub.service.impl;
+
+/**
+ * @author lichongbing
+ * @email 873610008@qq.com
+ * @date 2021/3/16 上午3:12
+ * cn.ruiyeclub.service.impl
+ */
+public class ServiceImpl<T, T1> {
+}
